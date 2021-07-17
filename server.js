d1900ec46c6c15e0cf2a64285a0a9473f6d27b54
Eshwari Learner <eshwarilearner@gmail.com>
2021-07-17 10:23:16 +0000

    Commit First Change

--- server.js
+++ server.js
@@ -177,5 +177,5 @@ app.use(express.static(__dirname + '/views'));
 
 var port = process.env.PORT || 8080
 app.listen(port, function () {
-  console.log("To view your app, open this link in your browser: http://localhost:" + port);
+  console.log("To view your application, open this link in your browser: http://localhost:" + port);
 });
